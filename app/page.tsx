4b75fb9b54bbfd1318b17ce3d30168be1553b65e
andy19910102 <andy19910102@gmail.com>
2025-07-19 08:00:41 +0800

    feat: upgrade nextjs react and typescript

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  // return () 裡面的即是用於表達網頁內容的HTML標籤
+  return (
+    <>
+      <h1 className="text-3xl font-bold my-20 text-center">你已成功開啟開發伺服器</h1>
+    </>
+  );
+}
--- components/Navbar.js
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-    return (
-        <nav className="w-full h-12">
-            <div className="container mx-auto">
-
-            </div>
-        </nav>
-    )
-}
